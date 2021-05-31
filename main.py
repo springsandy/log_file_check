e32886aff8b96ae143b95d913f3d589ea3b5f271
gitlabKim BomI <gitlabspringsandybtob@gmail.com>
2021-05-31 15:55:31 +0900

    파일에 log 출력

---
+++ main.py
@@ -0,0 +1,25 @@
+import logging
+from file import file_r
+
+def main():
+    logger = logging.getLogger(__name__)
+
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(filename)s:%(lineno)s %(message)s')
+
+    streamHandler = logging.StreamHandler()
+    fileHandler = logging.FileHandler('./a.log')
+
+    streamHandler.setFormatter(formatter)
+    fileHandler.setFormatter(formatter)
+
+    logger.addHandler(streamHandler)
+    logger.addHandler(fileHandler)
+
+    logger.setLevel(level=logging.DEBUG)
+    logger.debug('my DEBUG log')
+    logger.info('my INFO log')
+    logger.warning('my WARNING log')
+    logger.error('my ERROR log')
+    logger.critical('my CRITICAL log')
+
+main()
