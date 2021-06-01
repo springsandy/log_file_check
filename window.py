89cc5bc0ac956fe734e9dfe923ecd8e6ed1eab5c
s2018w26 <s2018w26>
2021-06-02 02:15:13 +0900

    창 상태바에 현재 날짜, 시간 표시

--- window.py
+++ window.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QDesktopWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtCore import QDateTime
 
 class MyApp(QMainWindow):
@@ -13,14 +13,7 @@ class MyApp(QMainWindow):
 
         self.setWindowTitle('log 확인하기')
         self.resize(400, 200)
-        self.center()
         self.show()
-
-    def center(self):
-        qr = self.frameGeometry()
-        cp = QDesktopWidget().availableGeometry().center()
-        qr.moveCenter(cp)
-        self.move(qr.topLeft())
 
 if __name__=='__main__':
     app = QApplication(sys.argv)
