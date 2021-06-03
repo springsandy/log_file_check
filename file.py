1813a39d73fb27e116f788df957cad94c235574c
gitlabKim BomI <gitlabspringsandybtob@gmail.com>
2021-06-03 17:59:12 +0900

    log_data에 line의 리스트 내용 추가하기

--- file.py
+++ file.py
@@ -1,5 +1,6 @@
 def file_r():
     f = open('./a.log', 'r')
+    log_data = []
     line_num = 1
 
     line = f.readline()
@@ -7,9 +8,13 @@ def file_r():
         line = line.rstrip('\n')
         line_data = line.split(', ')
         print("%d %s %s %s %s %s\n" %(line_num, line_data[0], line_data[1], line_data[2], line_data[3], line_data[4]), end='')
+        log_data.append(line_data)
         line = f.readline()
         line_num += 1
 
     print("총 수정해야될 부분의 개수", line_num ,"입니다.")
+    print(log_data)
 
     f.close()
+
+    return log_data
