cff5a069ef98e4bf53a7eafd6fae2cbdb2f3db77
s2018w26 <s2018w26>
2021-06-05 22:47:26 +0900

    입력받은 숫자로 level type 알아내기

--- choice.py
+++ choice.py
@@ -1,3 +1,7 @@
 def choice():
+    level_data =['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']
+
     print("1=>DEBUG / 2=>INFO / 3=>WARNING / 4=>ERROR / 5=>CRITICAL")
     level = input("이 다섯가지 중에 한가지를 선택하시오. -> ")
+
+    print(level_data[int(level)-1])
